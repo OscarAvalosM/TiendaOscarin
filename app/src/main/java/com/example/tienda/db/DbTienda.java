abb3a7b90146a23e7ce576fce4bf8cfaa07d83de
OscarAvalosM <88244248+OscarAvalosM@users.noreply.github.com>
2022-03-23 23:20:46 -0600

    Initial commit

---
+++ app/src/main/java/com/example/tienda/db/DbTienda.java
@@ -0,0 +1,43 @@
+package com.example.tienda.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import androidx.annotation.Nullable;
+
+import java.net.ContentHandler;
+
+public class DbTienda extends DbHelper{
+
+    Context context;
+
+    public DbTienda(@Nullable Context context) {
+        super(context);
+        this.context = context;
+    }
+    public long insertatienda(String cliente, String pedido, String telefono, String envio ){
+
+        long id = 0;
+
+        try {
+
+            DbHelper dbHelper = new DbHelper(context);
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+            ContentValues values = new ContentValues();
+            values.put("cliente", cliente);
+            values.put("pedido", pedido);
+            values.put("telefono", telefono);
+            values.put("envio", envio);
+
+            id = db.insert(TABLE_PEDIDO, null, values);
+        } catch(Exception ex){
+            ex.toString();
+        }
+
+
+
+        return  id;
+    }
+}
